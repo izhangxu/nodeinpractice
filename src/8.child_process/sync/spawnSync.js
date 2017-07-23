6a150659b836f60f96325ef96cb9aebb72ef5f41
zhangxu <zhangxu_wo@163.com>
2017-07-23 13:10:48 +0800

    update

--- src/8.child_process/sync/spawnSync.js
+++ src/8.child_process/sync/spawnSync.js
@@ -1,8 +1,8 @@
 var spy = require('child_process').spawnSync;
 var ps = spy('ps', ['aux']);
 var grep= spy('grep', ['node'], {
-	input: ps.stdin,
+	input: ps.stdout,
 	encoding: 'utf8'
 });
 
-console.log(grep);
+console.log(grep);      
